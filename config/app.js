a53c72ba1ce3599102471517d96bc0dd27d3750b
ezacoantonio <ezacoantonio@gmail.com>
2023-10-10 18:19:50 -0400

    setting up the app.js file with the required connection string to the database.

---
+++ config/app.js
@@ -0,0 +1,14 @@
+const express  = require('express');
+const indexRouter = require('../apps/routes/index');
+
+const app = express();
+
+app.use(express.json());
+app.use('./', indexRouter);
+
+// Connect to MongoDB without including the collection name in the URL
+mongoose.connect('mongodb+srv://appuser:appuser@cluster0.dgrqmgc.mongodb.net/DressStore', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB:', err));
+
+module.exports = app;
